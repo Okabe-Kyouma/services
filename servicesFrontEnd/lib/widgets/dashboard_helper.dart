5f222892413f24383f855fbf2fb5e3175cde4806
Okabe-Kyouma <ayushpal5432@gmail.com>
2024-11-11 20:28:49 +0530

    SearchBarAnd_SeachFunctionalityAdded

--- servicesFrontEnd/lib/widgets/dashboard_helper.dart
+++ servicesFrontEnd/lib/widgets/dashboard_helper.dart
@@ -1,47 +1,80 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:services/widgets/category_class.dart';
 
-class DashboardHelper extends StatelessWidget {
+class DashboardHelper extends StatefulWidget {
   const DashboardHelper({super.key});
 
   @override
+  State<DashboardHelper> createState() => _DashboardHelperState();
+}
+
+class _DashboardHelperState extends State<DashboardHelper> {
+  List<Map<String, String>> listOfWork = [
+    {"AC Technician": "assets/images/ac.jpeg"},
+    {"Babysitter/Nanny": "assets/images/nanny.jpeg"},
+    {"Barber": "assets/images/barber.jpeg"},
+    {"Beautician": "assets/images/beauty.jpeg"},
+    {"Carpenter": "assets/images/carpentar.jpeg"},
+    {"Chef": "assets/images/chef.jpeg"},
+    {"Cleaner": "assets/images/cleaner.jpeg"},
+    {"Construction Worker": "assets/images/construction.jpeg"},
+    {"Computer Technician": "assets/images/computer.jpeg"},
+    {"Digital Artist": "assets/images/digital.jpeg"},
+    {"Driver": "assets/images/driver.jpeg"},
+    {"Electrician": "assets/images/electrician.jpeg"},
+    {"Gardener": "assets/images/gardener.jpeg"},
+    {"Handyman": "assets/images/handyman.jpeg"},
+    {"Home Tutor": "assets/images/homeTutor.jpeg"},
+    {"House Cleaner": "assets/images/houseCleaner.jpeg"},
+    {"Laundry Service": "assets/images/laundry.jpeg"},
+    {"Mechanic": "assets/images/mechanic.jpeg"},
+    {"Mover": "assets/images/movers.jpeg"},
+    {"Painter": "assets/images/painter.jpeg"},
+    {"Pest Control": "assets/images/pest.jpeg"},
+    {"Personal Trainer": "assets/images/fitness.jpeg"},
+    {"Photographer": "assets/images/photo.jpeg"},
+    {"Plumber": "assets/images/plumber.jpeg"},
+    {"Security Guard": "assets/images/security.jpeg"},
+    {"Tailor": "assets/images/tailor.jpeg"},
+    {"Welder": "assets/images/welder.jpeg"},
+    {"Yoga Instructor": "assets/images/yoga.jpeg"}
+  ];
+
+  List<Map<String, String>> _filteredList = [];
+  final FocusNode _focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _filteredList = listOfWork;
+  }
+
+  void changeList(String value) {
+    List<Map<String, String>> results = [];
+
+    if (value.isEmpty) {
+      results = listOfWork;
+      _focusNode.unfocus();
+    } else {
+      results = listOfWork.where(
+        (element) {
+          return element.keys.first
+              .toLowerCase()
+              .trim()
+              .contains(value.toLowerCase().trim());
+        },
+      ).toList();
+    }
+    setState(() {
+      _filteredList = results;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-
-
-   List<Map<String, String>> listOfWork = [
-      {"AC Technician": "assets/images/ac.jpeg"},
-      {"Babysitter/Nanny": "assets/images/nanny.jpeg"},
-      {"Barber": "assets/images/barber.jpeg"},
-      {"Beautician": "assets/images/beauty.jpeg"},
-      {"Carpenter": "assets/images/carpentar.jpeg"},
-      {"Chef": "assets/images/chef.jpeg"},
-      {"Cleaner": "assets/images/cleaner.jpeg"},
-      {"Construction Worker": "assets/images/construction.jpeg"},
-      {"Computer Technician": "assets/images/computer.jpeg"},
-      {"Digital Artist": "assets/images/digital.jpeg"},
-      {"Driver": "assets/images/driver.jpeg"},
-      {"Electrician": "assets/images/electrician.jpeg"},
-      {"Gardener": "assets/images/gardener.jpeg"},
-      {"Handyman": "assets/images/handyman.jpeg"},
-      {"Home Tutor": "assets/images/homeTutor.jpeg"},
-      {"House Cleaner": "assets/images/houseCleaner.jpeg"},
-      {"Laundry Service": "assets/images/laundry.jpeg"},
-      {"Mechanic": "assets/images/mechanic.jpeg"},
-      {"Mover": "assets/images/movers.jpeg"},
-      {"Painter": "assets/images/painter.jpeg"},
-      {"Pest Control": "assets/images/pest.jpeg"},
-      {"Personal Trainer": "assets/images/fitness.jpeg"},
-      {"Photographer": "assets/images/photo.jpeg"},
-      {"Plumber": "assets/images/plumber.jpeg"},
-      {"Security Guard": "assets/images/security.jpeg"},
-      {"Tailor": "assets/images/tailor.jpeg"},
-      {"Welder": "assets/images/welder.jpeg"},
-      {"Yoga Instructor": "assets/images/yoga.jpeg"}
-    ];
-
-
     return PopScope(
       canPop: false,
       onPopInvoked: (didPop) async {
@@ -66,59 +99,82 @@ class DashboardHelper extends StatelessWidget {
           ),
         );
       },
-      child: Center(
-        child: ListView.builder(
-          itemCount: listOfWork.length,
-          itemBuilder: (context, index) {
-            String imagePath = listOfWork[index].values.first;
-            String text = listOfWork[index].keys.first;
-            return InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        CategoryClass(text: text, imageLink: imagePath),
-                  ),
-                );
+      child: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(20),
+            child: SearchBar(
+              focusNode: _focusNode,
+              leading: const Icon(Icons.search_rounded),
+              hintText: 'Electrician , plumber....',
+              autoFocus: false,
+              keyboardType: TextInputType.name,
+              onTapOutside: (event) {
+                _focusNode.unfocus();
               },
-              child: Card(
-                child: Stack(
-                  children: [
-                    Hero(
-                      tag: text,
-                      child: Image.asset(
-                        imagePath,
-                        fit: BoxFit.contain,
+              onChanged: (value) {
+                changeList(value);
+              },
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: ListView.builder(
+                itemCount: _filteredList.length,
+                itemBuilder: (context, index) {
+                  String imagePath = _filteredList[index].values.first;
+                  String text = _filteredList[index].keys.first;
+                  return InkWell(
+                    onTap: () {
+                      _focusNode.unfocus();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              CategoryClass(text: text, imageLink: imagePath),
+                        ),
+                      );
+                    },
+                    child: Card(
+                      child: Stack(
+                        children: [
+                          Hero(
+                            tag: text,
+                            child: Image.asset(
+                              imagePath,
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                          Container(
+                            height: 55,
+                            width: double.infinity,
+                            margin: const EdgeInsets.only(top: 170),
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                                color: Colors.black.withOpacity(0.6)),
+                            child: Text(
+                              text,
+                              style: const TextStyle(
+                                  color: Colors.white, fontSize: 25),
+                            ),
+                          )
+                          // Positioned(
+                          //   top: 190,
+                          //   left: 115,
+                          //   child: Text(
+                          //     text,
+                          //     style: TextStyle(fontSize: 25),
+                          //   ),
+                          // ),
+                        ],
                       ),
                     ),
-                    Container(
-                      height: 55,
-                      width: double.infinity,
-                      margin: const EdgeInsets.only(top: 170),
-                      alignment: Alignment.center,
-                      decoration:
-                          BoxDecoration(color: Colors.black.withOpacity(0.6)),
-                      child: Text(
-                        text,
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 25),
-                      ),
-                    )
-                    // Positioned(
-                    //   top: 190,
-                    //   left: 115,
-                    //   child: Text(
-                    //     text,
-                    //     style: TextStyle(fontSize: 25),
-                    //   ),
-                    // ),
-                  ],
-                ),
+                  );
+                },
               ),
-            );
-          },
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
